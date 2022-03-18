80e245587f12970be03cd091ddecfe17b54dbab8
Norsker <egn@magenta-aps.dk>
2022-03-18 14:19:32 +0100

    [#48611] ignore rules added

--- run-perlcritic.sh
+++ run-perlcritic.sh
@@ -17,13 +17,29 @@ if ! command -v "${cmd}" >/dev/null 2>&1; then
 fi
 
 cfg=.perlcriticrc
-opts=("--quiet")
+opts=("--quiet" "--color")
 if [[ ! -r "${cfg}" ]] && [[ ! -r "$HOME/${cfg}" ]]; then
-    opts+=("--stern" "--verbose" "8")
+    echo "No ${cfg} found"
+    exit 1
 fi
 
+declare -a exclusions=(
+"Perl::Critic::Policy::Modules::RequireExplicitPackage" # as we do not have the entire file, evaluating this rule makes no sense
+"Perl::Critic::Policy::TestingAndDebugging::RequireUseStrict" # as we do not have the entire file, evaluating this rule makes no sense
+"Perl::Critic::Policy::TestingAndDebugging::RequireUseWarnings" # as we do not have the entire file, evaluating this rule makes no sense
+"Perl::Critic::Policy::Modules::RequireEndWithOne" # as we do not have the entire file, evaluating this rule makes no sense
+)
+for exclusion in "${exclusions[@]}"
+do
+    opts+=("--exclude="$exclusion)
+done
+
+
 failed=false
-output=$(git diff --staged | "${cmd}" "${opts[@]}" --exclude Perl::Critic::Policy::TestingAndDebugging::RequireUseStrict)
+filtered_input=$(git diff --staged --color=always | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/' )
+output=$(echo $filtered_input | "${cmd}" "${opts[@]}")
+# filtered = $(perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/')
+
 if [[ ! -z "${output}" ]]; then
     echo "${output}"
     failed=true
