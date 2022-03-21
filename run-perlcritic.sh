359af39c54eb99ae89e94c9e478fe87718513e85
Norsker <egn@magenta-aps.dk>
2022-03-21 11:12:44 +0100

    works on a per file basis for clearer indiction of error location, added color

--- run-perlcritic.sh
+++ run-perlcritic.sh
@@ -6,9 +6,8 @@
 # Run perlcritic on perl scripts.
 # By default perlcritic looks for .perlcriticrc in the current directory
 # (repository root) and in the home directory. If no configuration file is
-# found then it uses --stern and --verbose 8.
-
-set -eux
+# found then it exits.
+set -u
 
 cmd=perlcritic
 if ! command -v "${cmd}" >/dev/null 2>&1; then
@@ -23,6 +22,7 @@ if [[ ! -r "${cfg}" ]] && [[ ! -r "$HOME/${cfg}" ]]; then
     exit 1
 fi
 
+# additional exclusions for quirks related to using a diff and not entire files
 declare -a exclusions=(
 "Perl::Critic::Policy::Modules::RequireExplicitPackage" # as we do not have the entire file, evaluating this rule makes no sense
 "Perl::Critic::Policy::TestingAndDebugging::RequireUseStrict" # as we do not have the entire file, evaluating this rule makes no sense
@@ -31,20 +31,30 @@ declare -a exclusions=(
 )
 for exclusion in "${exclusions[@]}"
 do
-    opts+=("--exclude="$exclusion)
+    opts+=("--exclude=$exclusion")
 done
 
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+failed="false"
+for changed_perl_file in "$@"; do
+    diff_input=$(git diff --staged --color=always "$changed_perl_file")
+    # filters staged diff to only include changed lines, https://stackoverflow.com/questions/25497881/git-diff-is-it-possible-to-show-only-changed-lines
+    # perl -wlne: -w=`warnings`, -l="newline at each line", -n="tells perl to implicitly include a loop as the second option", -e=`execute`
+    filtered_input=$( echo "$diff_input"  | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/' )
+    my_output=$(echo "$filtered_input" | "${cmd}" "${opts[@]}")
+    if [[ -n "${my_output}" ]]; then
+        echo "***************************"
+        echo
+        echo -e "${ORANGE}Found error(s) in diff of ${CYAN}$changed_perl_file ${NC}"
+        echo "${filtered_input}" | cat -n
+        echo "${my_output}"
+        echo
+        failed="true"
+    fi
+done
 
-failed=false
-filtered_input=$(git diff --staged --color=always | perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/' )
-output=$(echo $filtered_input | "${cmd}" "${opts[@]}")
-# filtered = $(perl -wlne 'print $1 if /^\e\[32m\+\e\[m\e\[32m(.*)\e\[m$/')
-
-if [[ ! -z "${output}" ]]; then
-    echo "${output}"
-    failed=true
-fi
-
-if [[ $failed == "true" ]]; then
+if [[ "${failed}" == "true" ]]; then
     exit 1
 fi
